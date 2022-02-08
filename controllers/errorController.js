cd82703e7b8a937b8e5e4d9f50c01fb90653f7ce
Denis Githuku <labrinthmcolly@gmail.com>
2022-02-08 05:48:48 +0300

    dev and prod error handlers

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,27 @@
-module.exports = (err, req, res, next) => {
-  console.log(err.stack);
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'fail';
+const sendErrorDev = (err, req, res, next) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    err: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, req, res, next) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
   });
+};
+
+module.exports = (err, req, res, next) => {
+  console.log(err.stack);
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'fail';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, req, res, next);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, req, res, next);
+  }
 };
